658ebd906ce206876899142eaf493d6c5d28980e
wuhaoming <wuhaoming@ghzhushou.com>
2022-07-13 14:54:41 +0800

    Feat: 完成基本逻辑

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,50 +1,168 @@
 <script setup lang="ts">
-const name = $ref('')
+interface BlockState {
+  x: number
+  y: number
+  revealed: boolean
+  mine?: boolean
+  figged?: boolean
+  adjacentMines: number
+}
 
-const router = useRouter()
-const go = () => {
-  if (name)
-    router.push(`/hi/${encodeURIComponent(name)}`)
+const WIDTH = 5
+const HEIGHT = 5
+const state = ref(Array.from({ length: HEIGHT }, (_, y) =>
+  Array.from({ length: WIDTH }, (_, x): BlockState => ({
+    x, y, adjacentMines: 0, revealed: false,
+  })),
+))
+
+function generateMines(initial: BlockState) {
+  for (const row of state.value) {
+    for (const block of row) {
+      if (Math.abs(block.x - initial.x) < 1)
+        continue
+      if (Math.abs(block.y - initial.y) < 1)
+        continue
+      block.mine = Math.random() < 0.2
+    }
+  }
+  updateNumbers()
+}
+
+const direction = [
+  [1, 1],
+  [1, 0],
+  [1, -1],
+  [0, -1],
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, 1],
+]
+
+const numberColors = [
+  'text-transparent',
+  'text-blue-500',
+  'text-gray-500',
+  'text-yellow-500',
+  'text-green-500',
+  'text-pink-500',
+  'text-emerald-500',
+  'text-indigo-500',
+  'text-cyan-500',
+]
+
+function updateNumbers() {
+  state.value.forEach((row, y) => {
+    row.forEach((block, x) => {
+      if (block.mine)
+        return
+
+      getSiblings(block).forEach((b) => {
+        if (b.mine)
+          block.adjacentMines += 1
+      })
+    })
+  })
+}
+
+function getSiblings(block: BlockState) {
+  return direction.map(([dx, dy]) => {
+    const x2 = block.x + dx
+    const y2 = block.y + dy
+    if (x2 < 0 || x2 >= WIDTH || y2 < 0 || y2 >= HEIGHT)
+      return undefined
+
+    return state.value[y2][x2]
+  }).filter(Boolean) as BlockState[]
+}
+
+function expandZero(block: BlockState) {
+  if (block.adjacentMines)
+    return
+
+  getSiblings(block).forEach((s) => {
+    if (!s.revealed) {
+      s.revealed = true
+      expandZero(s)
+    }
+  })
+}
+
+let mineGenerated = false
+const dev = false
+
+function onRightClick(block: BlockState) {
+  if (block.revealed)
+    return
+  block.figged = !block.figged
+}
+
+function onClick(e: MouseEvent, block: BlockState) {
+  if (!mineGenerated) {
+    generateMines(block)
+    mineGenerated = true
+  }
+  if (block.mine)
+    alert('BOOM!')
+
+  block.revealed = true
+  expandZero(block)
+}
+
+function getBlockClass(block: BlockState) {
+  if (block.figged)
+    return 'bg-gray-500/10 '
+  if (!block.revealed)
+    return 'bg-gray-500/10 hover:bg-gray-500/20'
+
+  return block.mine ? 'bg-red-500/50' : numberColors[block.adjacentMines]
+}
+
+watchEffect(checkGameState)
+
+function checkGameState() {
+  if (!mineGenerated)
+    return
+
+  const blocks = state.value.flat()
+  if (blocks.every(b => b.revealed || (b.figged && b.mine))) {
+    if (blocks.some(b => (b.figged && !b.mine)))
+      alert('lose')
+    else
+      alert('win')
+  }
 }
 </script>
 
 <template>
   <div>
-    <div i-carbon-campsite text-4xl inline-block />
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
-        Vitesse Lite
-      </a>
-    </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
-
-    <div py-4 />
-
-    <input
-      id="input"
-      v-model="name"
-      placeholder="What's your name?"
-      type="text"
-      autocomplete="false"
-      p="x-4 y-2"
-      w="250px"
-      text="center"
-      bg="transparent"
-      border="~ rounded gray-200 dark:gray-700"
-      outline="none active:none"
-      @keydown.enter="go"
-    >
-
-    <div>
-      <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
-      >
-        Go
-      </button>
+    minesweeper
+    <div p5>
+      <div v-for="(row, y) in state" :key="y" flex="~" justify-center items-center>
+        <button
+          v-for="(block, x) in row"
+          :key="x"
+          flex="~"
+          justify-center items-center
+          w-10 h-10
+          m="0.5"
+          border="1 gray-400/10"
+          :class="getBlockClass(block)"
+          @click="onClick($event, block)"
+          @contextmenu.prevent="onRightClick(block)"
+        >
+          <template v-if="block.figged">
+            <div i-mdi:flag text-red />
+          </template>
+          <template v-else-if="block.revealed || dev">
+            <div v-if="block.mine" i-mdi:mine />
+            <div v-else>
+              {{ block.adjacentMines }}
+            </div>
+          </template>
+        </button>
+      </div>
     </div>
   </div>
 </template>
